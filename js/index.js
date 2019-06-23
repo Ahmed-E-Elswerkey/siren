077ed00c1ae975906f69fb42a0403f2458919045
Ahmed Elswerkey <khawater11212@gmail.com>
2019-06-23 12:40:34 +0200

    display reqs from C-R

--- js/index.js
+++ js/index.js
@@ -30,7 +30,6 @@ function getLocation(e = ()=>{}){
       // timeout at 60000 milliseconds (60 seconds)
       var options = {timeout:60000};
       navigator.geolocation.getCurrentPosition(e, errorHandler,options);
-      //    e.innerHTML = 'Got it'
     } else{
         alert("Sorry, browser does not support geolocation!");
     }
@@ -40,7 +39,7 @@ function sendReq(e){
     getLocation((position)=>{
         long = position.coords.longitude
         lat = position.coords.latitude
-        ref.child('requests').push({
+        ref.child('Customers Requests').push({
             info:e.children[0].value,
             lat:lat,
             long:long,
@@ -50,11 +49,6 @@ function sendReq(e){
     })
 }
 var lat,long,uid, reqsRev
-// function showLocation(position) {
-//     var latitude = lat = position.coords.latitude;
-//     var longitude = long = position.coords.longitude;
-//     console.log(lat + ' ' + long)
-//  }
 var vehicelsNum = 0,tot=0
 window.onload = ()=>{
     firebase.auth().onAuthStateChanged(u=>{
@@ -67,13 +61,13 @@ window.onload = ()=>{
                 $I('st-name').innerHTML = m.val().name
             })
             ref.child('stations/'+uid+'/vehicles').on('value',m=>{
-                setTimeout(()=>{[].forEach.call($C('vehicle'),e=>{
-                    e.remove()
-                    cl('remove vehicle')
-                })},200)
+                // [].forEach.call($C('vehicle'),e=>{
+                //     e.remove()
+                // })
+                // $I('pop-vehicles').innerHTML = '<option value="">Select a vehicle</option>'
                 vehicelsNum = 0
                 tot=0
-                m.forEach(d=>{cl('asdas');tot++
+                m.forEach(d=>{tot++
                     if(d.val().status == '0'){
                         vehicelsNum++
                         var op = dc('option')
@@ -82,21 +76,22 @@ window.onload = ()=>{
                         op.setAttribute('data-key',d.key)
                         op.value = op.innerHTML += d.val().number
                         $I('pop-vehicles').appendChild(op)
-                        cl('add vehicle '+d.val().number)
                     }
                 })
                 if(vehicelsNum == 0 && $I('reqs') != undefined){
-                    $I('reqs').innerHTML = '<center>No vehicels available, requests are hidden</center>'
-                    ref.child('requests').once('value',n=>{
-                        n.forEach(d=>{
+                    $I('reqs').innerHTML = '<center>No vehicels available, Customers Requests are hidden</center>'
+                    ref.child('stations/'+uid).update({vehNum:'0'})
+                    cl('veh num 0')
+                    ref.child('Customers Requests').once('value',n=>{
+                        n.forEach(d=>{cl('cleaning')
                             if(d.val().status == '1' && d.val().nearestStation == uid)
-                                ref.child('requests/'+d.key).update({nearestStation:'',status:'0'})
+                                ref.child('Customers Requests/'+d.key).update({nearestStation:'',status:'0'})
                         })
                     })
                 }
                 else if(vehicelsNum > 0  && $I('reqs') != undefined){
+                    ref.child('stations/'+uid).update({vehNum:vehicelsNum})
                     forcedDisplay()
-                    cl('num > 0')
                 }
                 $I('veh-num').innerHTML = 'number of vehicles available = '+vehicelsNum + ' from '+tot + ' registered'
             })
@@ -109,7 +104,7 @@ window.onload = ()=>{
 }
 
 var req,sta
-var requests = [],stations = [],indexes = []
+var Requests = [],stations = [],indexes = []
 function prepareReqs(a){
     ref.child('stations').once('value',m=>{
         m.forEach(d=>{
@@ -118,6 +113,7 @@ function prepareReqs(a){
                 "long":d.val().long,
                 "key":d.key
             }
+            if(d.val().vehNum != '0')
             stations.push(cache)
         })
         sta = stations
@@ -128,66 +124,24 @@ function prepareReqs(a){
 
 function displayReqs(){
     cl('display')
-    ref.child('requests').orderByChild('key').on('child_added',n=>{
+    ref.child('Customers Requests').orderByChild('key').on('child_added',n=>{
         if(vehicelsNum>0){
-            // reqBody(n);
             calcDistance(n)
+            cl('calcing')
         }
         
     })
-    ref.child('requests').orderByChild('key').on('value',n=>{
+    ref.child('Customers Requests').orderByChild('key').on('value',n=>{
         $I('reqs').innerHTML=''
         n.forEach(i=>reqBody(i))
     })
 }
 
 function forcedDisplay(){
-    ref.child('requests').once('value',i=>{
+    ref.child('Customers Requests').once('value',i=>{
         i.forEach(n=>{
             if(n.val().status == '1')
                 calcDistance(n)
-            // if(n.val().nearestStation == uid){
-            //     $I('reqs').innerHTML = ''
-            //     var req = dc('div'),
-            //     info = dc('p'),
-            //     loc = dc('div'),
-            //     respond = dc('button')
-
-            //     // map = new google.maps.Map(loc, {
-            //     //     center: {lat:Number(n.val().lat), lng: Number(n.val().long)},
-            //     //     zoom: 14
-            //     // });
-
-            //     latLng = new google.maps.LatLng(Number(n.val().lat), Number(n.val().long))
-            //     var mapOptions = {
-            //         center: latLng,
-            //         zoom: 14,
-            //         mapTypeId: google.maps.MapTypeId.ROADMAP
-            //     };
-            //     var map = new google.maps.Map(loc, mapOptions);
-
-            //     var marker = new google.maps.Marker({
-            //         position: latLng,
-            //         title:"Destination",
-            //         visible: true
-            //     });
-            //     marker.setMap(map);
-
-            //     info.innerHTML = n.val().info
-            //     respond.setAttribute('for', 'pop-checkbox')
-            //     respond.innerHTML = 'Respond'
-            //     respond.setAttribute("onclick","\
-            //         $I('pop-info').innerHTML = '"+n.val().info+"';\
-            //         $I('pop-sub').setAttribute('data-key','"+n.key+"');\
-            //         toggle($I('pop'))")
-                
-
-            //     req.appendChild(info)
-            //     req.appendChild(loc)
-            //     req.appendChild(respond)
-            //     $I('reqs').appendChild(req)
-            //     $I('reqs').appendChild(req)
-            // }
         })
 
     })
@@ -195,21 +149,26 @@ function forcedDisplay(){
 
 function calcDistance(d){
     cl('child added')
-    if(d.val().status == '0'){
-        // var cache = {
-        //     "lat":d.val().lat,
-        //     "long":d.val().long,
-        //     "key":d.key
-        // }
-        var sta1 = new Array(),org1 = new google.maps.LatLng(d.val().lat, d.val().long)
+    data=[];da=0
+    if((d.val().status == '0' || d.val().status == undefined) && vehicelsNum > 0){cl('caling')
+        var long,lat
+        if(d.val().info == undefined){
+            d.val().l.forEach(d=>{
+                data[da] = d
+                da++
+            })
+            lat = data[0],long = data[1]
+            cl('data from g child ' + lat + ' ' + long)
+        }
+        else {
+            lat = d.val().lat,long = d.val().long
+            cl('data from info child')
+        }
+        var sta1 = new Array(),org1 = new google.maps.LatLng(lat,long)
 
         stations.forEach(e=>{
             sta1.push(new google.maps.LatLng(e.lat, e.long))
         })
-        // var origin1 = new google.maps.LatLng(a.origin.lat, a.origin.long);
-
-        // var destinationA = new google.maps.LatLng(50.087692, 14.421150),destinationB = new google.maps.LatLng(49.087692, 14.421150),destinationC = new google.maps.LatLng(51.087692, 14.421150);
-
         var service = new google.maps.DistanceMatrixService();
 
         service.getDistanceMatrix(
@@ -220,61 +179,87 @@ function calcDistance(d){
         }, callback);
 
         function callback(response, status) {
-            var i=0,s = response.rows[0].elements[0].distance.value,stationIndex=0
-            response.rows[0].elements.forEach(f=>{
-                // cl(f.distance.value)
-                if(f.distance.value < s){
-                    s = f.distance.value
-                    stationIndex = i%stations.length
-                }
-                i++
-            })
-            ref.child('requests/'+d.key).update({
-                nearestStation:stations[stationIndex].key,
-                status:'1'
-            })
-            var cachee = {[d.key]:stationIndex}
-            indexes.push(cachee)
+                var i=0,s = response.rows[0].elements[0].distance.value,stationIndex=0
+                response.rows[0].elements.forEach(f=>{
+                    cl(f.distance.value)
+                    if(f.distance.value < s){
+                        s = f.distance.value
+                        stationIndex = i%stations.length
+                    }
+                    i++
+                })
+                ref.child('Customers Requests/'+d.key).update({
+                    nearestStation:stations[stationIndex].key,
+                    status:'1'
+                })
+                var cachee = {[d.key]:stationIndex}
+                indexes.push(cachee)
         }
     }
 }
 
-var on=false
+var on=false,data=[],da=0
 function reqBody(n){
+    data=[];da=0
     if(n.val().status == '1')
-    if(n.val().nearestStation == uid){
+    if(n.val().nearestStation == uid && vehicelsNum > 0){
         var req = dc('div'),
         info = dc('p'),
         loc = dc('div'),
         respond = dc('button')
 
-        // map = new google.maps.Map(loc, {
-        //     center: {lat:Number(n.val().lat), lng: Number(n.val().long)},
-        //     zoom: 14
-        // });
-
-        latLng = new google.maps.LatLng(Number(n.val().lat), Number(n.val().long))
-        var mapOptions = {
-            center: latLng,
-            zoom: 14,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(loc, mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: latLng,
-            title:"Destination",
-            visible: true
-        });
-        marker.setMap(map);
-
-        info.innerHTML = n.val().info
-        respond.setAttribute('for', 'pop-checkbox')
-        respond.innerHTML = 'Respond'
-        respond.setAttribute("onclick","\
-            $I('pop-info').innerHTML = '"+n.val().info+"';\
-            $I('pop-sub').setAttribute('data-key','"+n.key+"');\
-            toggle($I('pop'))")
+        if(n.val().info != undefined){
+            latLng = new google.maps.LatLng(Number(n.val().lat), Number(n.val().long))
+            var mapOptions = {
+                center: latLng,
+                zoom: 14,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(loc, mapOptions);
+    
+            var marker = new google.maps.Marker({
+                position: latLng,
+                title:"Destination",
+                visible: true
+            });
+            marker.setMap(map);
+    
+            info.innerHTML = n.val().info
+            respond.setAttribute('for', 'pop-checkbox')
+            respond.innerHTML = 'Respond'
+            respond.setAttribute("onclick","\
+                $I('pop-info').innerHTML = '"+n.val().info+"';\
+                $I('pop-sub').setAttribute('data-key','"+n.key+"');\
+                toggle($I('pop'))")
+        }
+        else{
+            n.val().l.forEach(d=>{
+                data[da] = d
+                da++
+            })
+            latLng = new google.maps.LatLng(Number(data[0]), Number(data[1]))
+            var mapOptions = {
+                center: latLng,
+                zoom: 14,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(loc, mapOptions);
+    
+            var marker = new google.maps.Marker({
+                position: latLng,
+                title:"Destination",
+                visible: true
+            });
+            marker.setMap(map);
+    
+            info.innerHTML = n.val().g
+            respond.setAttribute('for', 'pop-checkbox')
+            respond.innerHTML = 'Respond'
+            respond.setAttribute("onclick","\
+                $I('pop-info').innerHTML = '"+n.val().g+"';\
+                $I('pop-sub').setAttribute('data-key','"+n.key+"');\
+                toggle($I('pop'))")
+        }
         
 
         req.appendChild(info)
@@ -287,7 +272,7 @@ function reqBody(n){
 
 function response(e){
     if(e.getAttribute('data-key') != undefined){
-        ref.child('requests/'+e.getAttribute('data-key')).update({
+        ref.child('Customers Requests/'+e.getAttribute('data-key')).update({
             status:'2',
             vehicle:$I('pop-vehicles').value
         })
@@ -296,44 +281,20 @@ function response(e){
         }).then(()=>{
             $I('pop').classList.add('hide')
             forcedDisplay()
-            ref.child('requests/'+e.getAttribute('data-key')).once('value',m=>{
-                cl($I('pop-vehicles').options[$I('pop-vehicles').selectedIndex].getAttribute('data-key'))
+            ref.child('Customers Requests/'+e.getAttribute('data-key')).once('value',m=>{
+                // cl($I('pop-vehicles').options[$I('pop-vehicles').selectedIndex].getAttribute('data-key'))
                 ref.child('accidents/'+$I('pop-vehicles').options[$I('pop-vehicles').selectedIndex].getAttribute('data-key')).set({
-                    lat:m.val().lat,
-                    long:m.val().long,
-                    info:m.val().info,
-                    status:m.val().status
+                    [m.key]:{
+                        lat:m.val().lat,
+                        longtude:m.val().long,
+                        info:m.val().info,
+                        status:m.val().status
+                    }
                 })
             })
         })
     }
 }
-
-
-// function distance(org,sta,f = ()=>{}){
-//     cl('asd')
-//     var sta1 = new Array(),org1 = new google.maps.LatLng(org.lat, org.long)
-
-//     sta.forEach(e=>{
-//         sta1.push(new google.maps.LatLng(e.lat, e.long))
-//     })
-//     // var origin1 = new google.maps.LatLng(a.origin.lat, a.origin.long);
-
-//     // var destinationA = new google.maps.LatLng(50.087692, 14.421150),destinationB = new google.maps.LatLng(49.087692, 14.421150),destinationC = new google.maps.LatLng(51.087692, 14.421150);
-
-//     var service = new google.maps.DistanceMatrixService();
-
-//     service.getDistanceMatrix(
-//     {
-//         origins: org1,
-//         destinations: sta1,
-//         travelMode: 'DRIVING'
-//     }, callback);
-
-//     function callback(response, status) {
-//         f(response.rows[0].elements)
-//     }
-// }
 
 
 function hideReg(){
@@ -374,24 +335,7 @@ function stationLog(e){
     })
 }
 
-// function distance(lat1, lon1, lat2, lon2, unit) {
-//     var radlat1 = Math.PI * lat1/180
-//     var radlat2 = Math.PI * lat2/180
-//     var radlon1 = Math.PI * lon1/180
-//     var radlon2 = Math.PI * lon2/180
-//     var theta = lon1-lon2
-//     var radtheta = Math.PI * theta/180
-//     var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-//     dist = Math.acos(dist)
-//     dist = dist * 180/Math.PI
-//     dist = dist * 60 * 1.1515
-//     if (unit=="K") { dist = dist * 1.609344 }
-//     if (unit=="N") { dist = dist * 0.8684 }
-//     return dist
-// } 
-
 function toggle(m){
-    cl('aa')
     if(m != undefined && !on){
         if(m.classList.contains('hide'))
             m.classList.remove('hide')
